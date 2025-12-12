45b6f7ebf45a377394a21aadebef78298c0bb891
pbc1001 <mintcho0328@gmail.com>
2025-12-12 13:56:24 +0900

    [Bronze V] Title: 긴자리 계산, Time: 96 ms, Memory: 14572 KB -BaekjoonHub

---
+++ Kotlin (JVM)/백준/Bronze/2338. 긴자리 계산/긴자리 계산.kt
@@ -0,0 +1,10 @@
+import java.math.BigInteger
+
+fun main() {
+    val a: BigInteger = readln().toBigInteger()
+    val b: BigInteger = readln().toBigInteger()
+
+    println(a + b)
+    println(a - b)
+    println(a * b)
+}
